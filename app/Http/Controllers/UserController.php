10a9f55f26f1be693938567d7b4fc14501f3afd8
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-06-12 22:58:59 +0800

    added the functionality in the UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
@@ -11,41 +14,71 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        // return UserResource::collection(User::latest()->simplePaginate(10));
+        $users = User::select('id', 'name', 'email', 'created_at')->latest()->simplePaginate(10);
+        return response([
+            'users' => $users->items(),
+            'pagination' => [
+                'current_page' => $users->currentPage(),
+                'per_page' => $users->perPage(),
+                'has_more' => $users->hasMorePages(),
+            ]
+        ]);
     }
-
-
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $attributes = $request->validate([
+            'name' => [
+                'required',
+                'string',
+            ],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Password::min(8)]
+        ]);
+        $attributes['password'] = bcrypt($attributes['password']);
+
+        $user = User::create($attributes);
+
+        return response([
+            'message' => 'User created successfully',
+            'data' => $user
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        return response([
+            'data' => $user
+        ]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $attributes = $request->validated([
+            'name' => [
+                'required',
+                'string',
+            ],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Password::min(8)]
+        ]);
+        if (isset($attributes['password'])) {
+            $attributes['password'] = bcrypt($attributes['password']);
+        }
+        $user->update($attributes);
+
+        return response([
+            'message' => "User updated successfully",
+            ''
+        ], 201);
     }
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'password',
     ];
 
+
     /**
      * The attributes that should be hidden for serialization.
      *
