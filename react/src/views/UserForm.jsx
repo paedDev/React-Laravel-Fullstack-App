296973f4d71718b5b5d72655cfabe9766b8d94c4
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-06-14 22:45:45 +0800

    can create a new user and delete user

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -1,8 +1,167 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Form, Link, useNavigate, useParams } from 'react-router-dom';
+import { GlobalContext } from '../context/ContextProvider';
+import axiosClient from '../axios-client';
 
 const UserForm = () => {
+  const { id } = useParams();
+  const { loading, setLoading } = useContext(GlobalContext);
+  const [errors, setErrors] = useState(null);
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    id: null,
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ""
+
+  });
+  const getUser = async () => {
+    setLoading(true);
+    try {
+      const res = await axiosClient.get(`/users/${id}`);
+
+      setUser(res.data.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      getUser();
+    }
+  }, [id]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setErrors(null);
+    try {
+      let response;
+      if (user.id) {
+        response = await axiosClient.put(`/users/${user.id}`, user);
+        setTimeout(() => {
+          navigate('/users');
+        }, 3000);
+      } else {
+        response = await axiosClient.post('/users', user);
+        setTimeout(() => {
+          navigate('/users');
+        }, 3000);
+      }
+    } catch (error) {
+      if (error.response.data.errors) {
+        setErrors(error.response.data.errors);
+      }
+    } finally {
+      setLoading(false);
+    }
+
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser(prev => ({
+      ...prev,
+      [name]: value
+    }));
+
+
+  };
+  const getError = (field) => {
+    if (!errors || !errors[field]) return null;
+    return Array.isArray(errors[field]) ? errors[field][0] : errors[field];
+  };
   return (
-    <div>UserForm</div>
+    <div>
+      <div className='mb-10'>
+        {
+          user.id && <h1 className='font-bold text-3xl '>Update User: {user.name}</h1>}
+        {!user.id && <h1 className='font-bold text-3xl '>New User</h1>}
+      </div>
+
+      <div className='fade-in'>
+        {
+          loading && (
+            <div className='text-center'>Loading...</div>
+          )
+        }
+      </div>
+      {
+        !loading &&
+        <form action="" onSubmit={handleSubmit} className='space-y-4 bg-gray-300/20 p-4 rounded-xl w-[90%] mx-auto h-auto'>
+          <div>
+            <label htmlFor="name" className='block text-sm font-medium'>Name</label>
+            <input type="text"
+              name='name'
+              value={user.name}
+              onChange={handleInputChange}
+              className='w-full block mt-1 rounded-md border border-gray-300 shadow-xl px-3 py-2 focus:outline-none focus:border-indigo-500 focus:ring-indigo-500'
+              placeholder='Name' />
+            {
+              getError('name') && (
+                <p className='text-red-500 mt-1 ml-1 '>{getError('name')}</p>
+              )
+            }
+          </div>
+          <div>
+            <label htmlFor="email" className='block text-sm font-medium'>Email</label>
+            <input type="email"
+              name='email'
+              value={user.email}
+              onChange={handleInputChange}
+              className='w-full block mt-1 rounded-md border border-gray-300 shadow-xl px-3 py-2 focus:outline-none focus:border-indigo-500 focus:ring-indigo-500'
+              placeholder='Email' />
+            {
+              getError('email') && (
+                <p className='text-red-500 mt-1 ml-1 '>{getError('email')}</p>
+              )
+            }
+          </div>
+          <div>
+            <label htmlFor="password" className='block text-sm font-medium'>Password</label>
+            <input type="password"
+              name='password'
+              value={user.password || ''}
+              onChange={handleInputChange}
+              className='w-full block mt-1 rounded-md border border-gray-300 shadow-xl px-3 py-2 focus:outline-none focus:border-indigo-500 focus:ring-indigo-500'
+              placeholder='Password' />
+            {
+              getError('password') && (
+                <p className='text-red-500 mt-1 ml-1 '>{getError('password')}</p>
+              )
+            }
+          </div>
+
+          <div>
+            <label htmlFor="password_confirmation" className='block text-sm font-medium'>Passwor Confimration</label>
+            <input type="password"
+              value={user.password_confirmation || ''}
+              name='password_confirmation'
+              onChange={handleInputChange}
+              className='w-full block mt-1 rounded-md border border-gray-300 shadow-xl px-3 py-2 focus:outline-none focus:border-indigo-500 focus:ring-indigo-500'
+              placeholder='Password Confirmation' />
+            {
+              getError('password_confirmation') && (
+                <p className='text-red-500 mt-1 ml-1 '>{getError('password_confirmation')}</p>
+              )
+            }
+
+          </div>
+          <div className='mt-2 space-x-2'>
+            <button type='submit ' className='bg-gray-800 text-white px-3 py-1 rounded-md hover:bg-gray-950 transition-colors duration-300 text-sm'>
+              Save
+            </button>
+            <Link to={'/users'} className='bg-gray-300 text-black font-medium px-3 py-1 rounded-md hover:bg-gray-400 transition-colors duration-300 text-sm inline-block'>
+              Cancel
+            </Link>
+          </div>
+
+        </form>
+      }
+
+    </div>
   );
 };
 
--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -4,7 +4,7 @@ import axiosClient from "../axios-client";
 import { Link } from 'react-router-dom';
 const Users = () => {
     const [users, setUsers] = useState([]);
-    const { loading, setLoading } = useContext(GlobalContext);
+    const { loading, setLoading, theme } = useContext(GlobalContext);
 
     useEffect(() => {
         getUsers();
@@ -15,6 +15,7 @@ const Users = () => {
         try {
             const response = await axiosClient.get('/users');
             console.log(response);
+            setUsers(response.data.users.data);
 
         } catch (error) {
 
@@ -22,11 +23,72 @@ const Users = () => {
             setLoading(false);
         }
     };
+    const onDelete = async (user) => {
+        if (!window.confirm("Are you sure you want to delete this user?")) {
+            return;
+        }
+        try {
+            const res = await axiosClient.delete(`/users/${user.id}`);
+            getUsers();
+        } catch (error) {
+
+        }
+    };
+    const themeClasses = {
+        container: theme === 'dark' ? 'bg-white/4' : 'bg-white/60',
+        tableHeader: theme === 'dark' ? 'bg-black/20 border-white/20' : 'border-gray-200 bg-gray-200',
+        tableBody: theme === 'dark' ? 'bg-black/2' : 'bg-white',
+        tableRow: theme === 'dark' ? 'hover:bg-gray-900' : 'hover:bg-gray-100',
+        text: theme === 'dark' ? 'text-gray-200' : 'text-gray-700'
+    };
     return (
-        <div >
-            <div className="flex justify-between items-center">
-                <h1>Users</h1>
-                <Link to={'/users/create'}> Add New Users </Link>
+        <div className="fade-in ease-in-out" >
+            <div className="flex justify-between items-center mb-6 ">
+                <h1 className="font-bold text-3xl">Users</h1>
+                <Link to={'/users/create'} className="px-4 py-1 bg-green-500 rounded text-white font-bold"> Add Users </Link>
+            </div>
+            <div className={`${themeClasses.container} p-8 rounded-lg shadow-xl max-w-7xl mx-auto`}>
+                <table className="w-full border-collapse ">
+                    <thead className={`${themeClasses.tableHeader} border-b  `}>
+                        <tr className="">
+                            <th className="px-6 py-4 text-left text-md font-semibold">ID</th>
+                            <th className="px-6 py-4 text-left text-md font-semibold">Email</th>
+                            <th className="px-6 py-4 text-left text-md font-semibold">Name</th>
+                            <th className="px-6 py-4 text-left text-md font-semibold"> Date</th>
+                            <th className="px-6 py-4 text-left text-md font-semibold">Actions</th>
+                        </tr>
+                    </thead>
+                    {loading && <tbody>
+                        <tr>
+                            <td colSpan={5} className="text-center text-2xl py-2">
+                                loading...
+                            </td>
+                        </tr>
+                    </tbody>}
+                    {!loading && <tbody className={`${themeClasses.tableBody}  divide-gray-600`}>
+                        {
+                            users.map((user) =>
+                            (
+                                <tr key={user.id} className={`${themeClasses.tableRow} border-b border-black/20transition-colors`}>
+                                    <td className={`${themeClasses.text} px-6 py-2 text-left `}>{user.id}</td>
+                                    <td className={`${themeClasses.text} px-6 py-2 text-left `}>{user.name}</td>
+                                    <td className={`${themeClasses.text} px-6 py-2 text-left `}>{user.email}</td>
+                                    <td className={`${themeClasses.text} px-6 py-2 text-left `}>{new Date(user.created_at).toLocaleDateString()}</td>
+                                    <td className={`${themeClasses.text} px-6 py-2 text-left `}>
+                                        <div className="flex justify-center items-center space-x-2">
+                                            <Link to={'/users/' + user.id} className="bg-blue-400 text-white px-4 py-1 rounded hover:bg-blue-500 text-sm font-medium transition-colors duration-500" >Edit</Link>
+                                            <button onClick={(e) => onDelete(user)} className="px-4 py-1 bg-red-400 text-white rounded font-medium text-sm hover:bg-red-500 transition-colors duration-300">
+                                                Delete
+                                            </button>
+                                        </div>
+
+                                    </td>
+                                </tr>
+                            )
+                            )
+                        }
+                    </tbody>}
+                </table>
             </div>
         </div>);
 };
